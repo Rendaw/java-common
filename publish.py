9005e6dfe3533c133ce9e1667d28c84f19b1049b
rendaw <spoo@zarbosoft.com>
2018-02-10 20:22:06 +0900

    Publish script work

--- publish.py
+++ publish.py
@@ -3,18 +3,27 @@ import subprocess
 import logging
 import argparse
 import os
+import re
+import xml.etree.ElementTree
 
 logging.basicConfig(level=logging.DEBUG)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('stack', choices=('production', 'development'))
 parser.add_argument('-f', '--force', action='store_true')
 args = parser.parse_args()
 
 if not args.force and subprocess.call(['git', 'diff-index', '--quiet', 'HEAD']) != 0:  # noqa
     raise RuntimeError('Working directory must be clean.')
 
-subprocess.check_call(['git', 'tag', args.version])
+with open('pom.xml', 'r') as pom_source:
+    pom_text = pom_source.read()
+pom_text = re.sub('xmlns="[^"]+"', '', pom_text)
+pom = xml.etree.ElementTree.parse('pom.xml').getroot()
+version = pom.find('//project/version').text
+print(version)
+1 / 0
+
+subprocess.check_call(['git', 'tag', 'v' + version])
 subprocess.check_call(['git', 'push', '--tags'])
 
 env = os.environ.copy()
